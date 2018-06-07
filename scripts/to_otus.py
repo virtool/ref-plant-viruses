b4c54f852b807089487ac5f3feae1cb91107192e
igboyes <igboyes@gmail.com>
2018-06-07 16:32:04 -0700

    Add to_otus.py

--- scripts/to_otus.py
+++ scripts/to_otus.py
@@ -3,7 +3,7 @@ import json
 import arrow
 import argparse
 
-parser = argparse.ArgumentParser(description="Rename virus.json files to kind.json")
+parser = argparse.ArgumentParser(description="Rename kind.json files to otu.json")
 
 parser.add_argument(
     "src",
@@ -16,12 +16,15 @@ args = parser.parse_args()
 src_path = args.src
 
 for alpha in os.listdir(src_path):
+        if alpha == "meta.json":
+            continue
+
         paths = [os.path.join(src_path, alpha, kind) for kind in os.listdir(os.path.join(src_path, alpha))]
 
         for path in paths:
-            if "virus.json" in os.listdir(path):
+            if "kind.json" in os.listdir(path):
                 os.rename(
-                    os.path.join(path, "virus.json"),
-                    os.path.join(path, "kind.json")
+                    os.path.join(path, "kind.json"),
+                    os.path.join(path, "otu.json")
                 )
             
