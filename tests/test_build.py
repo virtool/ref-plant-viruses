53e4842b542c45f14d5e8bdb2040e4dd815155af
igboyes <igboyes@gmail.com>
2019-02-18 13:03:23 -0800

    Update tests

--- tests/test_build.py
+++ tests/test_build.py
@@ -5,7 +5,10 @@ import shutil
 import pytest
 import subprocess
 
-TEST_FILE_PATH = "tests/files/reference.json"
+TEST_PATH = "tests/files/reference.json"
+TEST_NO_INDENT_PATH = "tests/files/reference_no_indent.json"
+TEST_NO_SCHEMA_PATH = "tests/files/reference_no_schema.json"
+TEST_WITH_INDENT_PATH = "tests/files/reference_with_indent.json"
 
 
 @pytest.mark.parametrize("version", [None, "v1.0.1", "v0.8.3"])
@@ -44,10 +47,16 @@ def test_created_at(tmpdir):
     assert (arrow.utcnow() - created_at).seconds == 0
 
 
-def test_divide_build(tmpdir):
-    shutil.copy(TEST_FILE_PATH, str(tmpdir))
-
+@pytest.mark.parametrize("indent", (True, False))
+@pytest.mark.parametrize("schema", (True, False))
+def test_divide_build(indent, schema, tmpdir):
+    
     json_path = os.path.join(str(tmpdir), "reference.json")
+
+    if schema:
+        shutil.copyfile(TEST_NO_INDENT_PATH, json_path)
+    else:
+        shutil.copyfile(TEST_NO_SCHEMA_PATH, json_path)    
 
     src_path = os.path.join(str(tmpdir), "src")
 
@@ -55,6 +64,26 @@ def test_divide_build(tmpdir):
 
     out_path = os.path.join(str(tmpdir), "out.json")
 
-    subprocess.call(["python", "scripts/build.py", "-f", out_path, src_path])
+    command = [
+        "python",
+        "scripts/build.py",
+        "-f", out_path,
+        src_path
+    ]
 
-    assert abs(os.path.getsize(json_path) - os.path.getsize(out_path)) < 10
+    if indent:
+        command.append("-i")
+
+    subprocess.call(command)
+
+    expected_path = TEST_NO_INDENT_PATH
+
+    if indent:
+        expected_path = TEST_WITH_INDENT_PATH
+
+    expected_size = os.path.getsize(expected_path)
+    out_size = os.path.getsize(out_path)
+
+    print(expected_size, out_size)
+
+    assert abs(expected_size - out_size) < 10
